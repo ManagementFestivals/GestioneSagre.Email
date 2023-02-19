4837e7094abd1633e28e7f78dd7a941edc2e90b5
Angelo Pirola <angelo9871@gmail.com>
2023-02-19 20:02:09 +0100

    Primo commit

---
+++ src/GestioneSagre.Email/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using GestioneSagre.Email.Controllers.Common;
+using GestioneSagre.Email.Messaging.Abstractions;
+using GestioneSagre.Email.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestioneSagre.Email.Controllers;
+
+public class EmailController : BaseController
+{
+    private readonly ILogger<EmailController> logger;
+    private readonly IMessageSender messageSender;
+
+    public EmailController(ILogger<EmailController> logger, IMessageSender messageSender)
+    {
+        this.logger = logger;
+        this.messageSender = messageSender;
+    }
+
+    [HttpPost("sendemail")]
+    public async Task<IActionResult> SendEmailAsync(EmailRequest request)
+    {
+        try
+        {
+            await messageSender.PublishAsync(request);
+            return Accepted();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
---
+++ src/GestioneSagre.Email/Startup.cs
@@ -0,0 +1,71 @@
+﻿using GestioneSagre.Email.Messaging.RabbitMq;
+using GestioneSagre.Email.Shared.Models;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.OpenApi.Models;
+
+namespace GestioneSagre.Email;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("GestioneSagre.Email", policy =>
+            {
+                policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(config =>
+        {
+            config.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Gestione Sagre Email",
+                Version = "v1"
+            });
+        });
+
+        services.Configure<KestrelServerOptions>(Configuration.GetSection("Kestrel"));
+
+        services.AddRabbitMq(settings =>
+        {
+            settings.ConnectionString = Configuration.GetConnectionString("RabbitMQ");
+            settings.ExchangeName = Configuration.GetValue<string>("AppSettings:ApplicationName");
+            settings.QueuePrefetchCount = Configuration.GetValue<ushort>("AppSettings:QueuePrefetchCount");
+        }, queues =>
+        {
+            queues.Add<EmailRequest>();
+        });
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        app.UseCors("GestioneSagre.Email");
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre Email v1");
+        });
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
