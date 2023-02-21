c92be8a0a89f1491ba9ca2216e960fb07b7b222e
Angelo Pirola <angelo9871@gmail.com>
2023-02-22 00:03:03 +0100

    Fix pacchetto Nuget

--- src/GestioneSagre.Email.Shared/Models/EmailSendResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace GestioneSagre.Email.Shared.Models;
-
-public class EmailSendResponse
-{
-    public bool Succeeded { get; set; }
-}
--- src/GestioneSagre.Email/GlobalUsings.cs
+++ src/GestioneSagre.Email/GlobalUsings.cs
@@ -1,8 +1,8 @@
 ﻿global using System.Net.Mime;
 global using GestioneSagre.Email.Controllers.Common;
-global using GestioneSagre.Email.Shared.Models;
 global using GestioneSagre.Messaging.Abstractions;
 global using GestioneSagre.Messaging.RabbitMq;
+global using GestioneSagre.SharedKernel.Models.Email;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Server.Kestrel.Core;
 global using Microsoft.OpenApi.Models;
